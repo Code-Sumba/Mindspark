f971b43abea5579d5c33e803699aaeab7703986d
AryanTheng <162592894+AryanTheng@users.noreply.github.com>
2025-01-16 12:23:50 +0530

    working project with gredle and auth update and auth not tested

---
+++ app/src/main/java/com/umang/MindzSpark/onboarding/GettingStartedActivity.kt
@@ -0,0 +1,23 @@
+package com.umang.MindzSpark.onboarding
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.umang.MindzSpark.R
+import com.umang.MindzSpark.auth.StudentDetailsActivity
+import kotlinx.android.synthetic.main.activity_getting_started.*
+
+class GettingStartedActivity : AppCompatActivity() {
+    @SuppressLint("ShowToast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_getting_started)
+
+        btnGetStarted.setOnClickListener {
+            val intent = Intent(this, StudentDetailsActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+            startActivity(intent)
+        }
+    }
+}
